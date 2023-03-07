dfc8a09c1c74a07d0cc9179bc4a1eddc3b3dc718
Adrian <y@thgie.ch>
2023-03-07 07:44:23 +0100

    fixed mobile viewport

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=1">
         <title>plusQ'île festival 2023</title>
 
         <link rel="shortcut icon" href="/assets/favicons/favicon.ico" type="image/x-icon" />

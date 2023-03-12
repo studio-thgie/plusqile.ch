c535b5563d041bccfd50cc9607f87ba05d84d444
Adrian <y@thgie.ch>
2023-03-12 17:56:11 +0100

    fixed artists listing

--- site/templates/artists.php
+++ site/templates/artists.php
@@ -5,7 +5,7 @@
     <main class="px-4 mx-auto max-w-7xl">
         <?php snippet('title', ['title' => $page->title()]); ?>
         <ul class="flex relative z-10 flex-col flex-wrap md:flex-row">
-            <?php foreach($pages->children() as $child): ?>
+            <?php foreach($page->children() as $child): ?>
                 <li class="block mb-4 md:mr-16 md:mb-12 md:inline-block"><a href="<?= $child->url() ?>" class="px-4 font-mono text-xl bg-white rounded-full border transition-colors md:px-6 md:text-7xl border-red hover:bg-red hover:text-white"><?= $child->title() ?></a></li>
             <?php endforeach ?>
         </ul>

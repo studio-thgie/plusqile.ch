a584cfc093b3d69e990cc7079b48543eea264059
Adrian <y@thgie.ch>
2023-05-03 11:49:04 +0200

    added option to add downloads to default pages

--- site/templates/default.php
+++ site/templates/default.php
@@ -36,6 +36,17 @@
                 <iframe class="mx-auto mt-4 mb-8 w-full max-w-5xl rounded-full border-2 border-red" src="https://snazzymaps.com/embed/439750" width="100%" height="600px"></iframe>
             <?php endif ?>
         <?php endforeach ?>
+
+        <?php if($page->downloads()->notEmpty()): ?>
+            <h2>Downloads</h2>
+            <ul class="">
+                <?php foreach ($page->downloads()->toFiles() as $key => $download): ?>
+                    <li class="mb-2">
+                        <a target="_blank" class="px-4 text-base list-none bg-white rounded-full border-2 transition-colors pointer-events-auto md:text-xl border-red hover:bg-red hover:text-white" style="text-decoration: none;" href="<?= $download->url() ?>"><?= ucwords(str_replace(['-', '_'], ' ', $download->name())) ?></a>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        <?php endif ?>
     </main>
 
 <?php snippet('footer'); ?>

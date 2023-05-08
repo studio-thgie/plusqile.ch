0455e8375cbb61c1b6beb208e8d3122442341b4f
Adrian <y@thgie.ch>
2023-05-08 20:17:38 +0200

    added possibility to add og image

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=1">
-        <title>plusQ'île festival 2023</title>
+        <title><?= $page->title() ?> &mdash; <?= $site->og_title() ?></title>
 
         <link rel="shortcut icon" href="/assets/favicons/favicon.ico" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/assets/favicons/apple-touch-icon.png" />
@@ -18,6 +18,19 @@
         <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon-180x180.png" />
 
         <link rel="stylesheet" href="/assets/styles/styles.css">
+
+        <meta property="og:title" content="<?= $page->title() ?> &mdash; <?= $site->og_title() ?>"/>
+        <?php if($site->og_description() != ''): ?>
+        <meta property="og:description" content="<?= $site->og_description() ?>"/>
+        <?php endif ?>
+        <?php if($site->og_image()->notEmpty()): ?>
+        <meta property="og:image" content="<?= $site->og_image()->toFile()->crop(1200, 630, [
+            'quality' => '90',
+            'crop' => 'center'
+        ])->url() ?>"/>
+        <meta property="og:image:width" content="1200"/>
+        <meta property="og:image:height" content="630"/>
+        <?php endif ?>
 
     </head>
     <body class="overflow-x-hidden">
